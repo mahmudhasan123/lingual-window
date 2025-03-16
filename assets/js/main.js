ac088847c9edf13f6b901cd8745800d948aad4e0
mahmudhasan123 <mahmudcodes@gmail.com>
2025-03-16 11:27:42 +0600

    show lesson levels from api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-console.log('main js file');
+function loadLessons() {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((data) => displayLessons(data.data));
+}
+
+function displayLessons(lessons) {
+  const lessonContainer = document.getElementById("lessonContainer");
+  for(let lesson of lessons){
+    const lessonsDiv = document.createElement('div');
+    lessonsDiv.innerHTML = `
+        <button
+            class="btn bg-transparent hover:bg-[#422AD520] text-[#422AD5] border-[#422AD5]"
+          >
+            <img src="assets/images/book-open.png" alt="" />
+            Lesson - ${lesson.level_no}
+          </button>
+    `;
+    lessonContainer.appendChild(lessonsDiv);
+  }
+}
+
+loadLessons();
